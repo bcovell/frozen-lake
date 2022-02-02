7f36ea2e9d905901d6d595c8f8eb8eb998a300f8
Bryan Covell <bryan.covell@heronsystems.com>
2022-02-02 11:41:17 -0500

    got q learning working

--- main.py
+++ main.py
@@ -1,41 +1,55 @@
 import gym
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def main():
     env: gym.Env = gym.make("FrozenLake-v1", is_slippery=False)
     env.render()
 
-    first_visit_mc(env, 1000, .99)
+    first_visit_mc(env, 100, .99)
 
 
 def first_visit_mc(env: gym.Env, num_episodes: int, discount: float):
-    value = np.zeros(env.observation_space.n)
-    returns = [[] for s in range(env.observation_space.n)]
-    for idx in range(num_episodes):
+    exploration = np.linspace(1, 0, num_episodes)
+    q_table = np.zeros([env.observation_space.n, env.action_space.n])
+    return_sums = np.zeros([env.observation_space.n, env.action_space.n])
+    return_counts = np.zeros([env.observation_space.n, env.action_space.n])
+    last_10_rewards = [0] * 10
+    avg_rewards = []
+    for ep_idx, epsilon in zip(range(num_episodes), exploration):
         states = []
         actions = []
         rewards = []
         states.append(env.reset())
         while True:
-            rand_action = env.action_space.sample()
-            new_state, reward, done, info = env.step(rand_action)
-            actions.append(rand_action)
+            if np.random.random() < epsilon:
+                action = env.action_space.sample()
+            else:
+                action = np.argmax(q_table[states[-1], :])
+            new_state, reward, done, info = env.step(action)
+            actions.append(action)
             rewards.append(reward)
             states.append(new_state)
-            # if done and rewards[-1] != 1:
-            #     rewards[-1] = -1
             if done:
                 break
+        last_10_rewards.pop(0)
+        last_10_rewards.append(reward)
+        avg_rewards.append(sum(last_10_rewards) / len(last_10_rewards))
         discounted_reward = 0
-        visited_states = []
-        for s, r in zip(states, rewards):
+        visited_state_actions = []
+        for s, a, r in zip(reversed(states[:-1]), reversed(actions),
+                           reversed(rewards)):
             discounted_reward = discount * discounted_reward + r
-            if s not in visited_states:
-                returns[s].append(discounted_reward)
-                value[s] = np.mean(returns[s])
-                visited_states.append(s)
-    print(value.reshape([4, 4], order='C'))
+            if s not in visited_state_actions:
+                return_sums[s, a] += discounted_reward
+                return_counts[s, a] += 1
+                q_table[s, a] = return_sums[s, a] / return_counts[s, a]
+                visited_state_actions.append((s, a))
+    plt.plot(np.arange(0, num_episodes), avg_rewards)
+    plt.xlabel('episodes')
+    plt.ylabel('average reward')
+    plt.show()
 
 
 if __name__ == '__main__':
